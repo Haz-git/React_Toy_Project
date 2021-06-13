eee34fb97100dea8014ac03ae82fffd246c37a8a
Haz-git <zhouharry1@gmail.com>
2021-06-13 06:57:34 -0700

    Creating input state

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div``;
+
+const MainInput = styled.input`
+    height: 2rem;
+    width: 4rem;
+`;
 
 const Input = () => {
-    return <div>test</div>;
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    return (
+        <MainContainer>
+            First Name:
+            <MainInput />
+            Last Name:
+            <MainInput />
+        </MainContainer>
+    );
 };
 
 export default Input;
