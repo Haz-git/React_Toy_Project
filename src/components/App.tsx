4ecd5ebb30982b976aab9c4cc4303c6b59f65c97
Haz-git <zhouharry1@gmail.com>
2021-06-13 06:57:18 -0700

    Added minor edits

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 //Components:
 import Input from './Input';
 
+//Styles:
+
 const App = () => {
     return (
         <div>
+            Context API Test
             <Input />
         </div>
     );
