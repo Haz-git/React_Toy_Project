a7b2522f808b9b18aec591109fa2e9760f89daa0
Haz-git <zhouharry1@gmail.com>
2021-06-14 14:40:55 -0700

    Added consumer

--- src/components/ChildContainerSub.tsx
+++ src/components/ChildContainerSub.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
+import { useContext } from 'react';
+import { nameContext } from './Input';
 
 const ChildContainerSub = () => {
+    //Grab the context object from input:
+    const grabbedContext = useContext(nameContext);
+    console.log(grabbedContext.firstName);
+
     return (
         <div>
             <div>
                 <h2>Child 2</h2>
             </div>
             This is a child of Child Container. This should render out the first name and last name.
+            <div>First Name : {grabbedContext.firstName}</div>
+            <div>Last Name : {grabbedContext.lastName}</div>
         </div>
     );
 };
