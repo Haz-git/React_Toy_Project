ae22510166e557af8d436138098f3a6d0fd0d9be
Haz-git <zhouharry1@gmail.com>
2021-06-14 14:03:59 -0700

    Added child 1

---
+++ src/components/ChildContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChildContainer = () => {
+    return <div>This is the direct child container to Input Parent Container</div>;
+};
+
+export default ChildContainer;
