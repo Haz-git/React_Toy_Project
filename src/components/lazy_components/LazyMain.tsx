5c19428283bd4ab8eed36db4de92df95b4682eeb
Haz-git <zhouharry1@gmail.com>
2021-06-14 15:34:13 -0700

    Added lazy loaded component

--- src/components/lazy_components/LazyMain.tsx
+++ src/components/lazy_components/LazyMain.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
+import { Suspense, lazy } from 'react';
+
+//Dynamic import:
+
+const LazyComponent = lazy(() => import('./LazyComponent'));
 
 const LazyMain = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div>
+                The component below is lazy loaded. To see the fallback 'LOADING...' in action, go
+                to chrome dev console and set '3g slow' network throttling.
+            </div>
+            <Suspense fallback={<div>LOADING...</div>}>
+                <LazyComponent />
+            </Suspense>
+        </div>
+    );
 };
 
 export default LazyMain;
