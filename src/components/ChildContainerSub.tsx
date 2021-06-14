910f64ed2c1f7f07ebac30e2e54da2dd943890cb
Haz-git <zhouharry1@gmail.com>
2021-06-14 14:18:13 -0700

    Added more details to container 2

--- src/components/ChildContainerSub.tsx
+++ src/components/ChildContainerSub.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 const ChildContainerSub = () => {
     return (
         <div>
+            <div>
+                <h2>Child 2</h2>
+            </div>
             This is a child of Child Container. This should render out the first name and last name.
         </div>
     );
