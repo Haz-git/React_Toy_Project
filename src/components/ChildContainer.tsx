25901786537200882ed9587f762c3de62cd4b532
Haz-git <zhouharry1@gmail.com>
2021-06-14 14:17:54 -0700

    Added more details to child container

--- src/components/ChildContainer.tsx
+++ src/components/ChildContainer.tsx
@@ -6,7 +6,11 @@ import ChildContainerSub from './ChildContainerSub';
 const ChildContainer = () => {
     return (
         <div>
-            This is the direct child container to Input Parent Container
+            <div>
+                <h2>Child 1</h2>
+            </div>
+            This is the direct child container to Input Parent Container: I shouldn't hold the first
+            name and last name!
             <ChildContainerSub />
         </div>
     );
