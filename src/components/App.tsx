ee0dca3594c7d32e0699ec84c31fd6271472cb31
Haz-git <zhouharry1@gmail.com>
2021-06-14 15:25:57 -0700

    Attached React Lazy and Suspense testing chamber

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 
 //Components:
 import Input from './Input';
+import LazyMain from './lazy_components/LazyMain';
 
 //Styles:
 const TestContainer = styled.div`
     border: 1px solid black;
     padding: 1em 1em;
+    margin: 1em 0;
 `;
 
 const App = () => {
@@ -16,6 +18,10 @@ const App = () => {
             <TestContainer>
                 <h1>Context API Test</h1>
                 <Input />
+            </TestContainer>
+            <TestContainer>
+                <h1>React Lazy + Suspense Test</h1>
+                <LazyMain />
             </TestContainer>
         </div>
     );
