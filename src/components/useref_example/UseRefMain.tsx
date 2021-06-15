186bb03cec8d4c469f76ee5a507c939feffc2f83
Haz-git <zhouharry1@gmail.com>
2021-06-15 14:47:46 -0700

    Added button with ref

--- src/components/useref_example/UseRefMain.tsx
+++ src/components/useref_example/UseRefMain.tsx
@@ -5,9 +5,20 @@ import styled from 'styled-components';
 const TestButton = styled.button``;
 
 const UseRefMain = () => {
+    const buttonCounter = useRef(0);
+
+    const handleButtonClick = () => {
+        buttonCounter.current++;
+        console.log(`You have clicked this button ${buttonCounter.current} times`);
+    };
+
+    console.log('This useRef based button has been rendered.');
+
     return (
         <div>
-            <TestButton>Log your button clicks.</TestButton>
+            <TestButton onClick={handleButtonClick}>
+                A useRef implemented button. Open your console and click this button.
+            </TestButton>
         </div>
     );
 };
