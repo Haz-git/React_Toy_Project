9b71cffe3f43085485c1a9543494353a5c0c0118
Haz-git <zhouharry1@gmail.com>
2021-06-16 15:16:03 -0700

    Added portals

--- src/components/portal_example/MainPortal.tsx
+++ src/components/portal_example/MainPortal.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const MainPortal = () => {
-    return <div>test</div>;
+    return (
+        <div>
+            A portal is rendered outside of the DOM structure, but does not affect the React
+            Components Tree.
+        </div>
+    );
 };
 
 export default MainPortal;
